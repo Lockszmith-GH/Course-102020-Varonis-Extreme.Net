465864a1f337701aabd67ef6ec3864541c51bf29
Gal Szkolnik <gszkolnik@varonis.com>
2020-10-13 13:42:46 -0400

    Added Error handling + using stash to preseve local changes

--- _git_update-main-from-upstream.ps1
+++ _git_update-main-from-upstream.ps1
@@ -1,9 +1,23 @@
 function performGitOperations {
-    git fetch upstream
-    git checkout main
-    git rebase upstream/main
-    git push origin main
-    git checkout gsz
+    try {
+        git stash
+        if( $LastExitCode -eq 1 ) { throw "FAILED: git stash"}
+        
+        git fetch upstream
+        if( $LastExitCode -eq 1 ) { throw "FAILED: git fetch upstream"}
+        git checkout main
+        if( $LastExitCode -eq 1 ) { throw "FAILED: git checkout main"}
+        git rebase upstream/main
+        if( $LastExitCode -eq 1 ) { throw "FAILED: git rebase upstream/main"}
+        git push origin main
+        if( $LastExitCode -eq 1 ) { throw "FAILED: git push origin main"}
+    } finally {
+        git checkout gsz
+        if( $LastExitCode -eq 1 ) { throw "FAILED: git checkout gsz"}
+        
+        git stash pop
+        if( $LastExitCode -eq 1 ) { throw "FAILED: git stash pop"}
+    }
 }
 
 performGitOperations
